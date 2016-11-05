64b33fc268a920713dabffd2219e27fc61560ce8
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-11-05 12:33:37 +0900

    [Add] 乱数の長さを指定する機能

--- popup_script.js
+++ popup_script.js
@@ -1,13 +1,23 @@
 (function () {
 	"use strict";
 	const randomStringInput = document.getElementById("random-string");
-	randomStringInput.value = create(30);
+	const randomStringLengthInput = document.getElementById("random-string-length");
+	const getRandomStringLength = () => parseInt(randomStringLengthInput.value);
 
+	randomStringLengthInput.addEventListener("keydown", evt => {
+		if (evt.key == "Enter") {
+			displayNewRandomString();
+		}
+	});
 
-	randomStringInput.focus();
-	randomStringInput.select();
+	function displayNewRandomString() {
+		randomStringInput.value = createRandomString(getRandomStringLength());
+		randomStringInput.focus();
+		randomStringInput.select();
+	}
+	displayNewRandomString();
 
-	function create(len) {
+	function createRandomString(len) {
 		const nums = "0123456789";
 		const chars_uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		const chars_lc = "abcdefghijklmnopqrstuvwxyz";
